7a9cef9d46c29f39ee83a62312e85604e34bbaec
Griffin Ryan <griffinryan@Griffins-MacBook-Pro.local>
2023-11-08 21:40:41 -0800

    last 20 points

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,34 @@
  * @author Griffin Ryan (glryan@uw.edu)
  */
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.SecureRandom;
 
 public class Main {
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder(2 * bytes.length);
+        for (byte b : bytes) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    private static byte[] generateRandomBytes(int length) {
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[length];
+        random.nextBytes(bytes);
+        return bytes;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -24,13 +47,17 @@ public class Main {
         String inputType = scanner.nextLine();
         
         byte[] inputBytes = null;
+        String inputText = "";
+        String filePath = "";
+        byte[] fileContent = null;
+
         if ("text".equalsIgnoreCase(inputType)) {
             System.out.println("Enter the text:");
-            String inputText = scanner.nextLine();
+            inputText = scanner.nextLine();
             inputBytes = inputText.getBytes();
         } else if ("file".equalsIgnoreCase(inputType)) {
             System.out.println("Enter the file path:");
-            String filePath = scanner.nextLine();
+            filePath = scanner.nextLine();
             try {
                 inputBytes = Files.readAllBytes(Paths.get(filePath));
             } catch (Exception e) {
@@ -78,7 +105,7 @@ public class Main {
                 System.out.println("Hashed output: " + bytesToHex(output));
             }
 
-        } else if(action.equals("M")){
+        } else if(action.equals("M")) {
 
             // Compute an authentication tag (MAC).
             System.out.println("Generating a MAC using KMACXOF256...");
@@ -93,7 +120,40 @@ public class Main {
             output = hash.KMACXOF256(keyBytes, inputBytes, 512, kmacCustomBytes);
 
         } else if(action.equals("E")) {
-            System.out.println("Not implemented yet.");
+            System.out.println("Enter the passphrase:");
+            String passphrase = scanner.nextLine();
+
+            if(inputType.equals("file")){
+                File file = new File(filePath);
+                fileContent = Files.readAllBytes(file.toPath());
+                
+            } else {
+                // Else, it is user-input text...
+
+            }
+            
+            // Encrypt a byte array m symmetrically under passphrase pw
+            byte[] z = generateRandomBytes(512);  // You need to implement a method to generate random bytes
+            byte[] concatenated = new byte[z.length + passphrase.getBytes().length];
+            System.arraycopy(z, 0, concatenated, 0, z.length);
+            System.arraycopy(passphrase.getBytes(), 0, concatenated, z.length, passphrase.getBytes().length);
+            
+            byte[] diversificationStringS = "S".getBytes(); // Convert the string to a byte array
+            byte[] keka = hash.KMACXOF256(concatenated, new byte[0], 1024, diversificationStringS);
+
+            byte[] ke = Arrays.copyOfRange(keka, 0, keka.length / 2);
+            byte[] ka = Arrays.copyOfRange(keka, keka.length / 2, keka.length);
+
+            // Now you need to obtain the byte array 'm' that you want to encrypt
+
+            byte[] diversificationStringSKE = "SKE".getBytes(); // Convert the string to a byte array
+            byte[] c = hash.KMACXOF256(ke, new byte[0], m.length * 8, diversificationStringSKE);
+
+            byte[] diversificationStringSKA = "SKA".getBytes(); // Convert the string to a byte array
+            byte[] tPrime = hash.KMACXOF256(ka, m, 512, diversificationStringSKA);
+
+            // The symmetric cryptogram is (z, c, t)
+            // Now handle the symmetric cryptogram as you wish.
 
         } else if(action.equals("D")) {
             System.out.println("Not implemented yet.");
@@ -103,15 +163,4 @@ public class Main {
         scanner.close();
     }
 
-    private static String bytesToHex(byte[] bytes) {
-        StringBuilder hexString = new StringBuilder(2 * bytes.length);
-        for (byte b : bytes) {
-            String hex = Integer.toHexString(0xff & b);
-            if (hex.length() == 1) {
-                hexString.append('0');
-            }
-            hexString.append(hex);
-        }
-        return hexString.toString();
-    }
 }
